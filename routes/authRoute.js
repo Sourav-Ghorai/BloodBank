fedaac7aa092d0c0118a028df6b29ca544812ffb
Sourav Ghorai <ghoraisourav451@gmail.com>
2024-07-14 08:39:45 +0530

    Added login controller

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { registerController } from "../controllers/authController.js";
+import { loginController, registerController } from "../controllers/authController.js";
 
 const router = express.Router();
 
 //Register
 router.post('/register', registerController);
+
+//Login
+router.post('/login', loginController);
 
 export default router;
